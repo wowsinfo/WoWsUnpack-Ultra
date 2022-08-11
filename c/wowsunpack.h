d4d01d30eb6f2a255efa016c75a979b24de51bff
Yiheng <development.henryquan@gmail.com>
2022-08-11 20:29:25 +1000

    updated header

--- c/wowsunpack.h
+++ c/wowsunpack.h
@@ -1,6 +1,9 @@
 #ifndef _WOWS_UNPACK_H_
 #define _WOWS_UNPACK_H_
 
-int cwowsunpack();
+/// Decode GameParams.data to GameParams.json
+/// path - a string pointing to the GameParams.data file
+/// compact - a boolean indicating the format mode
+int cwowsunpack(char*, int);
 
 #endif
