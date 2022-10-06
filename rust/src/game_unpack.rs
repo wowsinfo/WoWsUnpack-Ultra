5b120221d7fc79b6627d1e205d62d873b174eb86
Yiheng <development.henryquan@gmail.com>
2022-10-06 22:51:42 +1100

    implemented some helper functions

--- rust/src/game_unpack.rs
+++ rust/src/game_unpack.rs
@@ -1,6 +1,12 @@
 use log::{error, info, warn};
-use std::{array, vec};
 use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::{array, vec};
+
+///
+/// GameFileUnpack.hpp
+/// 
 
 type Byte = u8;
 
@@ -53,4 +59,67 @@ struct TreeNode {
 struct Unpacker {
     directory_tree: DirectoryTree,
     pkg_path: String,
+}
+///
+/// GameFileUnpack.cpp
+///
+
+const G_SEP: char = '\\';
+
+///
+/// Helpers
+/// 
+
+fn write_file_data(file_name: &str, data: &[Byte]) -> bool {
+    // write the data
+    match OpenOptions::new().write(true).create(true).open(file_name) {
+        Ok(mut file) => match file.write_all(data) {
+            Ok(_) => true,
+            Err(e) => {
+                error!("Failed to write data to outfile {} - {}", file_name, e);
+                false
+            }
+        },
+        Err(e) => {
+            error!("Failed to open file for writing {} - {}", file_name, e);
+            false
+        }
+    }
+}
+
+fn read_null_terminated_string(data: &[Byte], offset: usize) -> Option<String> {
+    let mut length = 0;
+
+    for i in offset..data.len() {
+        if data[i] == 0 {
+            length = i - offset;
+            break;
+        }
+    }
+
+    if length == 0 {
+        error!("Invalid String Length");
+        return None;
+    }
+
+    let string_data = &data[offset..(offset + length)];
+    let string = take_string(string_data, length);
+    if string.is_none() {
+        error!("Failed to read string");
+        return None;
+    }
+
+    return string;
+}
+
+fn take_string(data: &[Byte], size: usize) -> Option<String> {
+    if data.len() >= size {
+        let string = String::from_utf8(data[0..size].to_vec());
+        if string.is_err() {
+            error!("Failed to convert string");
+            return None;
+        }
+        return Some(string.unwrap());
+    }
+    return None;
 }
