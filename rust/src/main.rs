5932febf662cd96de1fa6af727108632076593e9
Yiheng <development.henryquan@gmail.com>
2022-10-18 22:48:29 +1100

    read game directory automatically

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,9 +2,16 @@ mod game_unpack;
 mod helper;
 mod text_unpack;
 
+extern crate log;
+use std::thread::panicking;
+
+use env_logger::Env;
+use game_directory::{GameDirectory, GameServer};
 use game_unpack::Unpacker;
 extern crate log;
 use env_logger::Env;
+
+mod game_directory;
 
 fn main() {
     if cfg!(debug_assertions) {
@@ -13,7 +20,20 @@ fn main() {
         env_logger::Builder::from_env(Env::default().default_filter_or("off")).init();
     }
 
-    let unpacker = Unpacker::new_auto(r"C:\Games\World_of_Warships").unwrap();
+    let mut game_dir = GameDirectory::new();
+    unsafe {
+        game_dir.locate();
+    }
+    game_dir.info();
+    let ww_dir = game_dir.get_game_directory(GameServer::WW);
+    if ww_dir.is_none() {
+        panic!("Failed to find World of Warships game directory");
+    }
+
+    let ww_dir = ww_dir.unwrap();
+    let unpacker = Unpacker::new_auto(ww_dir).unwrap();
+    unpacker.extract("gui/dogTags/medium/", "output").unwrap();
+    unpacker.extract("gui/4k/", "output").unwrap();
     unpacker
         .extract_exact("gui/dogTags/medium/", "output")
         .unwrap();
@@ -27,11 +47,7 @@ fn main() {
 mod tests {
     use crate::game_unpack::{GameLanguages, Unpacker};
     use crate::text_unpack::MoFileReader;
-
-    #[test]
-    fn dummy() {
-        assert_eq!(1, 1);
-    }
+    use crate::game_directory::{GameDirectory, GameServer};
 
     #[test]
     fn test_unpacker_new() {
@@ -85,6 +101,39 @@ mod tests {
         assert!(reader.is_ok());
         let reader = reader.unwrap();
         let result = reader.write_to_file("ja.json".to_string(), "output".to_string());
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn find_game_directory() {
+        let mut game_dir = GameDirectory::new();
+        unsafe {
+            game_dir.locate();
+        }
+        let ww_dir = game_dir.get_game_directory(GameServer::WW);
+        assert!(ww_dir.is_some());
+        game_dir.info();
+    }
+
+    #[test]
+    fn test_unpack_with_auto_game_directory() {
+        let mut game_dir = GameDirectory::new();
+        unsafe {
+            game_dir.locate();
+        }
+        let ww_dir = game_dir.get_game_directory(GameServer::WW);
+        assert!(ww_dir.is_some());
+
+        let ww_dir = ww_dir.unwrap();
+        let unpacker = Unpacker::new_auto(ww_dir);
+        assert!(unpacker.is_ok());
+
+        let unpacker = unpacker.unwrap();
+        let result = unpacker.extract("gui/dogTags/medium/", "output");
+        assert!(result.is_ok());
+        let result = unpacker.extract("gui/4k/", "output");
+        assert!(result.is_ok());
+        let result = unpacker.extract("content/GameParams.data", "output");
         assert!(result.is_ok());
     }
 }
