55fa9987d4805b0ed2f91008d4900d57498be296
Yiheng <development.henryquan@gmail.com>
2022-11-04 23:02:53 +1100

    added c GameServer enum

--- c/wowsunpack.h
+++ c/wowsunpack.h
@@ -1,6 +1,13 @@
 #ifndef _WOWS_UNPACK_H_
 #define _WOWS_UNPACK_H_
 
+// Match with Rust Game Server
+enum GameServer {
+    GAME_SERVER_WW = 0,
+    GAME_SERVER_CN = 1,
+    GAME_SERVER_PT = 2,
+};
+
 /// Decode GameParams.data to GameParams.json
 /// path - a string pointing to the GameParams.data file
 /// compact - a boolean indicating the format mode
