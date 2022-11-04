f6de898bfc4624c0fb442fcd1e6a7e51c5b7bdab
Yiheng <development.henryquan@gmail.com>
2022-11-04 22:59:37 +1100

    unpack languages in parallel
    it is about 2 to 3 times faster

--- wowsunpacker/src/export/tools.rs
+++ wowsunpacker/src/export/tools.rs
@@ -1,5 +1,11 @@
-use crate::{game::{GameServer, GameDirectory, GameLanguages}, types::UnpackResult, unpacker::{LangUnpacker, GameUnpacker}};
+use log::{error, info};
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 
+use crate::{
+    game::{GameDirectory, GameLanguages, GameServer},
+    types::UnpackResult,
+    unpacker::{GameUnpacker, LangUnpacker},
+};
 
 pub fn unpack_languages(server: GameServer, dest: &str) -> UnpackResult<()> {
     let ww_dir = GameDirectory::new()
@@ -8,13 +14,48 @@ pub fn unpack_languages(server: GameServer, dest: &str) -> UnpackResult<()> {
         .ok_or("Failed to find World of Warships game directory")?;
 
     let unpacker = GameUnpacker::auto(&ww_dir)?;
-    for lang in GameLanguages::values().iter() {
-        println!("Unpacking language: {}", lang);
-        let lang_dir = unpacker.get_lang_path(lang);
-        LangUnpacker::new(lang_dir)?
-            .decode()?
-            .write_to_file(&lang.to_filename(), dest)?;
-    }
+    // for lang in GameLanguages::values().iter() {
+    //     println!("Unpacking language: {}", lang);
+    //     let lang_dir = unpacker.get_lang_path(lang);
+    //     LangUnpacker::new(lang_dir)?
+    //         .decode()?
+    //         .write_to_file(&lang.to_filename(), dest)?;
+    // }
+
+    // unpack all languages in parallel
+    // GameLanguages::values().par_iter().for_each(|lang| {
+    //     info!("Unpacking language: {}", lang);
+    //     let lang_dir = unpacker.get_lang_path(lang);
+    //     match LangUnpacker::new(lang_dir) {
+    //         Ok(mut unpacker) => {
+    //             match unpacker.decode() {
+    //                 Ok(unpacker) => {
+    //                     match unpacker.write_to_file(&lang.to_filename(), dest) {
+    //                         Ok(_) => info!("Unpacked language: {}", lang),
+    //                         Err(e) => error!("Failed to write language file: {}", e),
+    //                     }
+    //                 },
+    //                 Err(e) => error!("Failed to decode language file: {}", e),
+    //             }
+    //         },
+    //         Err(e) => error!("Failed to create language unpacker: {}", e),
+    //     }
+    // });
+
+    GameLanguages::values()
+        .par_iter()
+        .try_for_each(|lang| -> Option<()> {
+            info!("Unpacking language: {}", lang);
+            let lang_dir = unpacker.get_lang_path(lang);
+            LangUnpacker::new(lang_dir)
+                .ok()?
+                .decode()
+                .ok()?
+                .write_to_file(&lang.to_filename(), dest)
+                .ok()?;
+            Some(())
+        })
+        .ok_or("Failed to unpack languages")?;
 
     Ok(())
 }
